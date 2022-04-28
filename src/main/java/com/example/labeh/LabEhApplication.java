af247396462f3acaf9154c048d4ab2bf3a397e92
DESKTOP-KUQ032B\anadr <anadragos98@gmail.com>
2022-04-28 20:44:31 +0300

    initial commit

---
+++ src/main/java/com/example/labeh/LabEhApplication.java
@@ -0,0 +1,13 @@
+package com.example.labeh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabEhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabEhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/labeh/LabEhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labeh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabEhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
